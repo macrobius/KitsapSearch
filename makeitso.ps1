f3b836ac1add33708b4fe9c7f546c2b0c56f98a5
Test Identity <jegoodwin3@gmail.com>
2020-08-19 10:07:06 -0700

    script to trigger workflow

---
+++ makeitso.ps1
@@ -0,0 +1,6 @@
+$projectName="KitsapSearchProject"
+$location="centralus"
+$resourceGroupName="${projectName}rg"
+$appName="http://${projectName}"
+$scope=$(az group create --name $resourceGroupName --location $location --query 'id')
+az ad sp create-for-rbac --name $appName --role Contributor --scopes $scope --sdk-auth
